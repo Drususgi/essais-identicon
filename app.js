3626ae56d4297e347e625a26ee345f94c1689299
Drususgi <ginluc@msn.com>
2018-05-26 12:06:45 +0200

    prems

---
+++ app.js
@@ -0,0 +1,12 @@
+
+new Vue({
+    el: '#app',
+    data: {
+        title: 'mlijmlkjlmkj'
+    },
+    methods: {
+      onInput: function() {
+        console.log('Someone typed something');
+      }
+    }
+  });
